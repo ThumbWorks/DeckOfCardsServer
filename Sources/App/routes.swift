053201b464f5849786f64880f9202cca0fc4e1e0
Roderic Campbell <roderic@gmail.com>
2020-04-06 19:35:32 -0700

    Cleanup the network request logic a bit

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,9 @@
 import Vapor
 import Foundation
 
+// TODO we need to get a proper path in the form of something like /tmp/generatedCode/githubUser/reponame/language
 private let pathToGeneratedCode = "/tmp/generatedCode"
+private let generatorServiceHost = "generator3.swagger.io"
 
 struct GenerationResponse: Content {
     var status: Bool
@@ -33,6 +35,7 @@ enum GenerationError: Error {
     case failedToCreateDirectory
     case failedToGenerateClientCode
     case failedToMoveGeneratedCode
+    case failedToUnzip
     case failedToRemoveArtifacts(String)
 
     var localizedDescription: String {
@@ -46,6 +49,8 @@ enum GenerationError: Error {
             return "Failed to move client code"
         case .failedToRemoveArtifacts(let file):
             return "Failed to remove code gen build artifacts. \(file)"
+        case .failedToUnzip:
+            return "Failed to unzip the payload"
         }
     }
 }
@@ -54,23 +59,15 @@ enum GenerationError: Error {
 public func routes(_ router: Router) throws {
     // Basic "It works" example
     router.post { req -> GenerationResponse in
-
-        let language = "swift5"
-
         let content = parseRequestJson(rawRequestContent: req.content)
 
         // Step 1: Make the tmp directory and fail silently
-
         try? makeCodeDirectory()
-        do {
-            try generateClient(owner: content.owner(), repo: content.repo(), version: content.version(), language: language)
-            try cleanupGeneratedCode()
-        } catch {
-            _ = say(someWord: error.localizedDescription)
-            return GenerationResponse(status: false, localizedString: error.localizedDescription)
-        }
-        _ = say(someWord: "Successfully built client!")
-
+        let specURLString = "https://api.swaggerhub.com/apis/\(content.owner())/\(content.repo())/\(content.version())/swagger.json"
+        let client = try buildHTTPClient(incomingRequest: req)
+        let requestData = try buildJSONPayload(specURLString: specURLString)
+        let outgoingRequest = buildHTTPRequest(with: requestData)
+        fetchGeneratedClient(with: req, httpClientFuture: client, outgoingRequest: outgoingRequest)
         return GenerationResponse(status: true, localizedString: "Successfully built client!")
     }
 
@@ -106,31 +103,37 @@ private func parseRequestJson(rawRequestContent: ContentContainer<Request>) -> W
     }
     return content
 }
+private func unzipPayload() throws {
+    guard shell("unzip", "-o", "\(pathToGeneratedCode)/client.zip", "-d", "\(pathToGeneratedCode)/client/") == 0 else {
+        throw GenerationError.failedToUnzip
+    }
+}
 /// mv /tmp/generatedCode/SwaggerClient/Classes/Swaggers/* /tmp/generatedCode/
 private func cleanupGeneratedCode() throws {
-    guard shell("cp", "-r", "\(pathToGeneratedCode)/SwaggerClient/Classes/Swaggers/", "\(pathToGeneratedCode)/") == 0 else {
+    // This is all very swift-client specific instructions
+    guard shell("cp", "-r", "\(pathToGeneratedCode)/client/SwaggerClient/Classes/Swaggers/", "\(pathToGeneratedCode)/") == 0 else {
         throw GenerationError.failedToMoveGeneratedCode
     }
-    guard shell("rm", "-r", "\(pathToGeneratedCode)/SwaggerClient/") == 0 else {
-//        guard shell("rm", "-r", "\(pathToGeneratedCode)/SwaggerClient/", "SwaggerClient.podspec", "git_push.sh", "Cartfile") == 0 else {
+    guard shell("rm", "-r", "\(pathToGeneratedCode)/client/SwaggerClient/") == 0 else {
         throw GenerationError.failedToRemoveArtifacts("Swagger Client directory")
     }
-    guard shell("rm", "\(pathToGeneratedCode)/SwaggerClient.podspec") == 0 else {
+    guard shell("rm", "\(pathToGeneratedCode)/client/SwaggerClient.podspec") == 0 else {
         throw GenerationError.failedToRemoveArtifacts("Pod spec file")
     }
-    guard shell("rm", "\(pathToGeneratedCode)/git_push.sh") == 0 else {
+    guard shell("rm", "\(pathToGeneratedCode)/client.zip") == 0 else {
+        throw GenerationError.failedToRemoveArtifacts("downloaded zip file")
+    }
+    guard shell("rm", "\(pathToGeneratedCode)/client/git_push.sh") == 0 else {
         throw GenerationError.failedToRemoveArtifacts("git push shell script")
     }
-
-    guard shell("rm", "\(pathToGeneratedCode)/Cartfile") == 0 else {
+    guard shell("rm", "\(pathToGeneratedCode)/client/Cartfile") == 0 else {
         throw GenerationError.failedToRemoveArtifacts("Cart file")
     }
 
 }
 
 //swagger-codegen generate -i https://api.swaggerhub.com/apis/Thumbworks/DeckOfCards/1.0.0/swagger.json -l swift5 -o /tmp/generatedCode/
-private func generateClient(owner: String, repo: String, version: String, language: String) throws {
-    let url = "https://api.swaggerhub.com/apis/\(owner)/\(repo)/\(version)/swagger.json"
+private func generateClient(url: String, language: String) throws {
     guard shell("swagger-codegen", "generate", "-i", url, "-l", language, "-o", pathToGeneratedCode) == 0 else {
         throw GenerationError.failedToGenerateClientCode
     }
@@ -142,11 +145,77 @@ private func makeCodeDirectory() throws {
     }
 }
 
+private func buildJSONPayload(specURLString: String) throws -> Data {
+    let bodyDict = [
+           "specURL": specURLString,
+           "lang" : "swift5",
+           "type" : "CLIENT",
+           "codegenVersion" : "V3"
+       ]
+    return try JSONSerialization.data(withJSONObject: bodyDict, options: .prettyPrinted)
+}
+
+private func buildHTTPRequest(with requestData: Data) -> HTTPRequest {
+    var httpReq = HTTPRequest(method: .POST, url: "/api/generate")
+    httpReq.contentType = MediaType.json
+    httpReq.body = HTTPBody(data: requestData)
+    return httpReq
+}
+
+private func buildHTTPClient(incomingRequest: Request) throws -> EventLoopFuture<HTTPClient> {
+    return HTTPClient.connect(scheme: .https, hostname: generatorServiceHost, port: nil, connectTimeout: .minutes(1), on: incomingRequest) { error in
+        print("We got an error in the connection \(error)")
+    }
+}
+
+private func fetchGeneratedClient(with incomingRequest: Request, httpClientFuture: EventLoopFuture<HTTPClient>, outgoingRequest: HTTPRequest) {
+    // Connect a new client to the supplied hostname.
+    // Send the HTTP request, fetching a response
+    let promise = incomingRequest.eventLoop.newPromise(Void.self)
+    /// Dispatch some work to happen on a background thread
+    DispatchQueue.global().async {
+        /// Puts the background thread to sleep
+        /// This will not affect any of the event loops
+        do {
+            let httpRes = try httpClientFuture.wait().send(outgoingRequest)
+            httpRes.do { (response) in
+
+                do {
+                    // TODO we need to put this in a network request specific tmp folder
+                    try response.body.data?.write(to: URL(fileURLWithPath: "\(pathToGeneratedCode)/client.zip"))
+                    try unzipPayload()
+                    // TODO Add the github integration here
+                    try cleanupGeneratedCode()
+                } catch GenerationError.failedToRemoveArtifacts(let string) {
+                    _ = say(someWord: "Error cleaning up artifacts \(string)")
+                    promise.fail(error: GenerationError.failedToRemoveArtifacts(string))
+                }
+                catch {
+                    print("error writing file \(error)")
+                    promise.fail(error: error)
+                    return
+                }
+            }.catch { (error) in
+                print("error parsing the response \(error)")
+                promise.fail(error: error)
+            }
+        } catch {
+            print("error with request \(error)")
+            promise.fail(error: error)
+        }
+        /// When the "blocking work" has completed,
+        /// complete the promise and its associated future.
+        promise.succeed()
+    }
+}
+/**
+ Speak status of either an error or success state during local development
+ */
 private func say(someWord: String) -> Bool {
     return shell("say", someWord) == 1
 }
 
-func shell(_ args: String...) -> Int32 {
+private func shell(_ args: String...) -> Int32 {
     let task = Process()
     // print(task.currentDirectoryPath)
     task.launchPath = "/usr/bin/env"
