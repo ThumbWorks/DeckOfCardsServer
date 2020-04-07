0ae4956f31479af7613a866597fbb157bbf3c396
Roderic Campbell <roderic@gmail.com>
2020-04-07 14:41:22 -0700

    Use a better futures centric approach to downloading the client

--- Sources/App/Controllers/GeneratedCodeFetchController.swift
+++ Sources/App/Controllers/GeneratedCodeFetchController.swift
@@ -109,45 +109,25 @@ final class GeneratedCodeFetchController {
         }
 
     }
-    private func fetchGeneratedClient(with incomingRequest: Request, httpClientFuture: EventLoopFuture<HTTPClient>, outgoingRequest: HTTPRequest) {
-        // Connect a new client to the supplied hostname.
-        // Send the HTTP request, fetching a response
-        let promise = incomingRequest.eventLoop.newPromise(Void.self)
-        /// Dispatch some work to happen on a background thread
-        DispatchQueue.global().async {
-            /// Puts the background thread to sleep
-            /// This will not affect any of the event loops
-            do {
-                let httpRes = try httpClientFuture.wait().send(outgoingRequest)
-                httpRes.do { (response) in
 
-                    do {
-                        // TODO we need to put this in a network request specific tmp folder
-                        try response.body.data?.write(to: URL(fileURLWithPath: "\(pathToGeneratedCode)/client.zip"))
-                        try self.unzipPayload()
-                        // TODO Add the github integration here
-                        try self.cleanupGeneratedCode()
-                    } catch GenerationError.failedToRemoveArtifacts(let string) {
-                        _ = self.say(someWord: "Error cleaning up artifacts \(string)")
-                        promise.fail(error: GenerationError.failedToRemoveArtifacts(string))
-                    }
-                    catch {
-                        print("error writing file \(error)")
-                        promise.fail(error: error)
-                        return
-                    }
-                }.catch { (error) in
-                    print("error parsing the response \(error)")
-                    promise.fail(error: error)
-                }
-            } catch {
-                print("error with request \(error)")
-                promise.fail(error: error)
-            }
-            /// When the "blocking work" has completed,
-            /// complete the promise and its associated future.
-            promise.succeed()
+
+    private func fetchGeneratedClient(client: Client, requestData: Data) {
+
+        let outgoingRequest = buildHTTPRequest(with: requestData)
+        let responseFuture = client.post("https://\(generatorServiceHost)/api/generate") { serverRequest in
+            serverRequest.http = outgoingRequest
         }
+        responseFuture.catch { error in
+            print("error \(error)")
+        }
+        _ = responseFuture.map { (response) -> (Void) in
+            // TODO probably catch these errors
+            try? response.http.body.data?.write(to: URL(fileURLWithPath: "\(pathToGeneratedCode)/client.zip"))
+            try? self.unzipPayload()
+            try? self.cleanupGeneratedCode()
+            // TODO Add the github integration here
+        }
+        return
     }
 
 
@@ -166,14 +146,6 @@ final class GeneratedCodeFetchController {
             throw GenerationError.failedToCreateDirectory
         }
     }
-
-    // probably not necessary, see Tim's code
-    private func buildHTTPClient(incomingRequest: Request) throws -> EventLoopFuture<HTTPClient> {
-        return HTTPClient.connect(scheme: .https, hostname: generatorServiceHost, port: nil, connectTimeout: .minutes(1), on: incomingRequest) { error in
-            print("We got an error in the connection \(error)")
-        }
-    }
-
 
     /**
       Parse the request content
@@ -201,7 +173,7 @@ final class GeneratedCodeFetchController {
     }
 
     private func buildHTTPRequest(with requestData: Data) -> HTTPRequest {
-        var httpReq = HTTPRequest(method: .POST, url: "/api/generate")
+        var httpReq = HTTPRequest(method: .POST, url: "https://\(generatorServiceHost)/api/generate")
         httpReq.contentType = MediaType.json
         httpReq.body = HTTPBody(data: requestData)
         return httpReq
@@ -213,10 +185,9 @@ final class GeneratedCodeFetchController {
         // Step 1: Make the tmp directory and fail silently
         try? makeCodeDirectory()
         let specURLString = "https://api.swaggerhub.com/apis/\(content.owner())/\(content.repo())/\(content.version())/swagger.json"
-        let client = try buildHTTPClient(incomingRequest: req)
+        let client = try req.client()
         let requestData = try buildJSONPayload(specURLString: specURLString)
-        let outgoingRequest = buildHTTPRequest(with: requestData)
-        fetchGeneratedClient(with: req, httpClientFuture: client, outgoingRequest: outgoingRequest)
+        fetchGeneratedClient(client: client, requestData: requestData)
         return GenerationResponse(status: true, localizedString: "Successfully built client!")
     }
 }
